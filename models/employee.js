fd08291fc3d6680c0e95fcde19b4f14b27fee96b
Kanchan Nag <84240298+kanchankk11@users.noreply.github.com>
2023-01-21 19:05:17 +0530

    rest API

---
+++ models/employee.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+const employeeSchema = new mongoose.Schema({
+    empId : {
+        type : Number,
+        required : true,
+        unique : [true, "EmpID already exists"]
+    },
+    name : {
+        type : String,
+        required : true,
+        minlength : 3
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : [true, "Email already exists"],
+        validate(value){
+            if(!validator.isEmail(value))
+                throw new Error("Invalid Email")
+        }
+    },
+    phone : {
+        type : Number,
+        required : true,
+        minlength : 10,
+        maxlength : 10
+    },
+    address : {
+        type : String,
+        required : true
+    }
+});
+
+//* Creating collection
+
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = Employee;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
 const express = require('express');
-require('')
+require('../db/connection')
+const Employee = require('../models/employee');
 const port = process.env.PORT || 3000;
 const app = express();
 
-
+app.use(express.json());
 app.get("/",(req, res) => {
     res.send("Hello");
+})
+
+app.post("/employee",(req, res) => {
+    //console.log(req.body);
+    const emp = new Employee(req.body);
+
+    if(emp == null)
+        res.status(500).json({
+            status : 500,
+            message : "Internal server error occured"
+        })
+    emp.save().then(()=>{
+        res.status(201).json({
+            status : 201,
+            message : "Inserted successfully."
+        })
+    }).catch((err)=> {
+        res.status(400).json({
+            status : 400,
+            message : err.message 
+        })
+    })
+   
+})
+
+app.get("/employee", async (req,res) => {
+   
+    let result = null;
+
+    try {
+        if(req.query.id)
+            result = await Employee.find({empId : req.query.id});
+        else
+            result = await Employee.find();
+
+        if(result == null || result.length == 0)
+        res.status(404).json({
+            status : 400,
+            message : "No data found."
+        });
+        
+        res.send(result)
+    } catch (error) {
+        console.log(error);
+    }
+    
 })
 
 app.listen(port, (err)=>{
